dc09dd88b5a7bf5d9be81e28edf8493df06e6d55
Michael Yowell <michael.yowell@gmail.com>
2017-06-21 16:08:52 -0400

    Implemented the file reading and parsing for the main method.

--- SudokuAssessment/Program.cs
+++ SudokuAssessment/Program.cs
@@ -5,11 +5,13 @@
 /// This program will attempt to solve each 9x9 Sudoku puzzle present in the 'puzzles' directory and represented as a .txt file.
 /// Solzed puzzles will be placed in the 'solutions' directory.
 /// Each solution file will be saved in the format of {0}.sln.txt where {0} is the name of the original puzzle file.
+/// It is assumed that each puzzle input will be 9x9.
 /// Author: Michael Yowell - michael.yowell@gmail.com
 /// </remarks>
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +19,36 @@ using System.Threading.Tasks;
 namespace SudokuAssessment {
     class Program {
         static void Main(string[] args) {
+            String puzzleDirectory = Properties.Resources.puzzleDirectoryPath; // Name of the directory that holds each Sudoku puzzle
+            String solutionsDirectory = Properties.Resources.solutionsDirectoryPath; // Name of the directory that will hold each solution file
+            String[] puzzleEntries; // Listing of file names found in the puzzle directory
+            List<char> charBuffer = new List<char>(); // Buffer to hold contents of a puzzle file
+
+            try {
+
+                // Load the name of each file in the puzzles directory
+
+                puzzleEntries = Directory.GetFiles(puzzleDirectory);
+
+                foreach(String fileName in puzzleEntries) {
+                    StreamReader puzzleStream = new StreamReader(fileName);
+
+                    // Read all contents minus returns and newlines into the string buffer
+
+                    while(puzzleStream.Peek() > 0){
+                        char c = (char)puzzleStream.Read();
+                        if (c != '\n' && c != '\r')
+                            charBuffer.Add(c);
+                    }
+
+                    charBuffer.ForEach(Console.WriteLine);
+                }
+                
+            } catch(Exception e) {
+                Console.Error.Write("Error accessing or reading puzzle files: {0}\n", e.Message);
+            }
+
+            Console.Read();
         }
     }
 }
--- SudokuAssessment/SudokuBoard.cs
+++ SudokuAssessment/SudokuBoard.cs
@@ -4,12 +4,33 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SudokuAssessment {
-    class SudokuBoard {
+    public class SudokuBoard {
+        private static int ROWS = 9;  // Number of rows in a Sudoku board
+        private static int COLUMNS = 9; // Number of columns in a Sudoku board
+
+        int[,] gameBoard = new int[ROWS, COLUMNS]; // Two dimensional array that will hold each value on the given Sudoku puzzle
+
+        /// <summary>
+        /// Default constructor for SudokuBoard
+        /// </summary>
+        /// <remarks>
+        /// Made private to prevent the invalid creation of an empty board
+        /// </remarks>
+        private SudokuBoard() { }
+
+        /// <summary>
+        /// Class constructor for SudokuBoard
+        /// </summary>
+        /// <param name="puzzleContents"></param>
+        public SudokuBoard(List<char> puzzleContents){
+
+        }
 
     }
 }
